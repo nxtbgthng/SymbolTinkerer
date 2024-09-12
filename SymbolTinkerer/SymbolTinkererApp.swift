9ececdc8781ce6206ecdeb02f4933f8fbaeeb5f8
Gernot Poetsch <github@poetsch.org>
2024-09-12 11:52:31 +0200

    Initial Commit

---
+++ SymbolTinkerer/SymbolTinkererApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolTinkererApp.swift
+//  SymbolTinkerer
+//
+//  Created by Gernot Poetsch on 12.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SymbolTinkererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
